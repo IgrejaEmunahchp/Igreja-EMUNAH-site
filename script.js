bba964212d5cc4ffd1650d3f00acca18bf6dcd81
IgrejaEmunah <rickboaretto13@gmail.com>
2026-07-01 14:32:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('EMUNAH');
+function enviar(){
+let n=document.getElementById('nome').value;
+let m=document.getElementById('msg').value;
+if(!n||!m){alert('Preencha tudo');return;}
+window.open('https://wa.me/556496078723?text='+encodeURIComponent(n+' - '+m));
+}
