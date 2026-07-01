5d62e4c902f518e3e869f2dc6ed4bd506abc3991
IgrejaEmunah <rickboaretto13@gmail.com>
2026-07-01 14:41:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function enviar(){
-let n=document.getElementById('nome').value;
-let m=document.getElementById('msg').value;
-if(!n||!m){alert('Preencha tudo');return;}
-window.open('https://wa.me/556496078723?text='+encodeURIComponent(n+' - '+m));
+let nome=document.getElementById("nome").value;
+let msg=document.getElementById("msg").value;
+
+if(!nome || !msg){
+alert("Preencha todos os campos");
+return;
 }
+
+window.open(
+"https://wa.me/556496078723?text=" +
+encodeURIComponent("🙏 Pedido de oração\nNome: "+nome+"\n\n"+msg)
+);
+}
+
+/* animação scroll */
+window.addEventListener("scroll",()=>{
+document.querySelectorAll(".reveal").forEach(el=>{
+let top = el.getBoundingClientRect().top;
+if(top < window.innerHeight - 100){
+el.classList.add("active");
+}
+});
+});
